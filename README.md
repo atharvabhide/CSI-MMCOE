bf72c717468c8b46fd6e09283fd7b5dbe59125d0
Atharva Bhide <67187699+atharvabhide@users.noreply.github.com>
2022-09-16 13:56:32 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,29 @@
+<img src = "https://github.com/atharvabhide/CSI-MMCOE/blob/main/static/images/CSI_logo.png" height="100" width="100"></img>
+# CSI-MMCOE
+
+
+# Usage
+
+Install dependencies (Tailwind)
+
+```
+npm install
+```
+
+Run the Tailwind CLI to compile the **input.css** during development. The output file is **css/main.css**
+
+```
+npm run watch
+```
+
+To build once run...
+
+```
+npm run build
+```
+
+To run website
+
+```
+python manage.py runserver
+```
---
+++ tailwind.config.js
@@ -0,0 +1,45 @@
+/** @type {import('tailwindcss').Config} */
+const plugin = require("tailwindcss/plugin");
+
+const Myclass = plugin(function ({ addUtilities }) {
+  addUtilities({
+    ".my-rotate-y-180": {
+      transform: "rotateY(180deg)",
+    },
+    ".preserve-3d": {
+      transformStyle: "preserve-3d",
+    },
+    ".perspective": {
+      perspective: "1000px",
+    },
+    ".backface-hidden": {
+      backfaceVisibility: "hidden",
+    },
+  });
+});
+
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      '2xl': '928px',
+      xl: '1440px'
+    },
+    extend: {
+      colors: {
+        brightRed: 'hsl(12, 88%, 59%)',
+        brightRedLight: 'hsl(12, 88%, 69%)',
+        brightRedSupLight: 'hsl(12, 88%, 95%)',
+        darkBlue: 'hsl(228, 39%, 23%)',
+        darkGrayishBlue: 'hsl(227, 12%, 61%)',
+        veryDarkBlue: 'hsl(233, 12%, 13%)',
+        veryPaleRed: 'hsl(13, 100%, 96%)',
+        veryLightGray: 'hsl(0, 0%, 98%)',
+      }
+    },
+  },
+  plugins: [Myclass],
+}
